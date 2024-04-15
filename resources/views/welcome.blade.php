0b84c87f1efa1b4064f87104a32ba5e01f745832
Azizul-Islam <azizul656589@gmail.com>
2024-04-15 12:46:29 +0600

    fonts download

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,13 +5,11 @@
     <meta charset="UTF-8">
     <link rel="icon" type="image/svg+xml" href="/favicon.png">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
-    <link rel="preload" href="/fonts/Mona-Sans.woff2" as="font" type="font/woff2" crossorigin="anonymous">
+    {{-- <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin=""> --}}
+    <link rel="preload" href="{{ asset('fonts/webfonts/MonaSans-Regular.woff2') }}" as="font" type="font/woff2" crossorigin="anonymous">
     <title>{{ $setting->meta_title }}</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
-        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="{{ asset('frontend/all.min.css') }}" />
     <link rel="stylesheet" href="{{ asset('./frontend/frame.css') }}">
     <style>
         .circlediv {
@@ -436,6 +434,7 @@
     
     <script src="{{ asset('frontend/jquery-3.7.1.min.js') }}"
         integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="{{ asset('frontend/all.min.js') }}"></script>
     <script src="{{ asset('frontend/html2canvas.js') }}"></script>
 
     <script>
